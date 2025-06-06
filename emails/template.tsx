b0c9560a09a1380640c7da5760a18600f5146621
josiah-praise <praisechinonso21@gmail.com>
2025-06-06 14:08:22 +0100

    updated email template

--- emails/template.tsx
+++ emails/template.tsx
@@ -45,7 +45,6 @@ const PREVIEW_DATA = {
   },
 };
 
-
 type emailTemplateProps = {
   userName: string;
   type: "monthly-report" | "budget-alert";
@@ -87,16 +86,26 @@ export default function EmailTemplate({
             <Section style={styles.statsContainer}>
               <div style={styles.stat}>
                 <Text style={styles.text}>Total Income</Text>
-                <Text style={styles.heading}>${data?.stats?.totalIncome}</Text>
+                <Text style={styles.heading}>
+                  &#8358;{data?.stats?.totalIncome}
+                </Text>
               </div>
               <div style={styles.stat}>
                 <Text style={styles.text}>Total Expenses</Text>
-                <Text style={styles.heading}>${data?.stats?.totalExpenses}</Text>
+                <Text style={styles.heading}>
+                  &#8358;{data?.stats?.totalExpenses}
+                </Text>
               </div>
               <div style={styles.stat}>
                 <Text style={styles.text}>Net</Text>
                 <Text style={styles.heading}>
-                  ${data?.stats?.totalIncome - data?.stats?.totalExpenses}
+                  {data?.stats?.totalIncome - data?.stats?.totalExpenses < 0
+                    ? "-"
+                    : "+"}
+                  &#8358;
+                  {Math.abs(
+                    data?.stats?.totalIncome - data?.stats?.totalExpenses
+                  )}
                 </Text>
               </div>
             </Section>
@@ -109,7 +118,7 @@ export default function EmailTemplate({
                   ([category, amount]) => (
                     <div key={category} style={styles.row}>
                       <Text style={styles.text}>{category}</Text>
-                      <Text style={styles.text}>${amount}</Text>
+                      <Text style={styles.text}> &#8358;{amount}</Text>
                     </div>
                   )
                 )}
@@ -154,16 +163,16 @@ export default function EmailTemplate({
             <Section style={styles.statsContainer}>
               <div style={styles.stat}>
                 <Text style={styles.text}>Budget Amount</Text>
-                <Text style={styles.heading}>${data?.budgetAmount}</Text>
+                <Text style={styles.heading}>&#8358;{data?.budgetAmount}</Text>
               </div>
               <div style={styles.stat}>
                 <Text style={styles.text}>Spent So Far</Text>
-                <Text style={styles.heading}>${data?.totalExpenses}</Text>
+                <Text style={styles.heading}>&#8358;{data?.totalExpenses}</Text>
               </div>
               <div style={styles.stat}>
                 <Text style={styles.text}>Remaining</Text>
                 <Text style={styles.heading}>
-                  ${data?.budgetAmount - data?.totalExpenses}
+                  &#8358;{data?.budgetAmount - data?.totalExpenses}
                 </Text>
               </div>
             </Section>
